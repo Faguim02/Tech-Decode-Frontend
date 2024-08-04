1c6b3c35b5e0e163a4afa15927fcabb87ed7f4f5
Faguim <fagnermuniz12@gmail.com>
2024-08-04 11:35:05 -0300

    feat(ui): create animate from components

--- src/components/PostComponent/index.tsx
+++ src/components/PostComponent/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 export const PostComponent = () => {
   return (
-    <article className='flex-col space-y-1 hover:scale-105 focus:border-2 focus:border-slate-400' style={{transition: '0.3s'}}>
+    <motion.article 
+    initial={{ scale: 0.5 }}
+    whileInView={{ scale: 1 }}
+    viewport={{ once: true }}
+    transition={{ type: "spring" }}
+    className='cursor-pointer flex-col space-y-1 hover:scale-105 focus:border-2 focus:border-slate-400' style={{transition: '0.3s'}}
+    >
         <img src="https://blog.runrun.it/wp-content/uploads/2024/02/IA-Generativa_Blog.png.webp" alt="a" className='rounded-lg object-cover' style={{
             height: '250px'
         }}/>
         <span className='text-slate-400'>04 mar 2024</span>
         <h2 className='text-lg font-bold text-slate-800'>Post Title</h2>
-    </article>
+    </motion.article>
   )
 }
