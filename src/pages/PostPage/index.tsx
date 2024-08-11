e3cf40d630f72a855235296dc93e61f753e808a5
Faguim <fagnermuniz12@gmail.com>
2024-08-11 20:32:10 -0300

    refactor: method axios createComment

--- src/pages/PostPage/index.tsx
+++ src/pages/PostPage/index.tsx
@@ -28,13 +28,13 @@ export const PostPage = () => {
     const newData = {
       comment: commentValue,
       name: user,
-      date: '',
+      date: '0',
       post_id: id
     } as commentDto
 
     const commentService = new CommentService()
-    const comment = await commentService.createComment(newData)
-    console.log(comment)
+    await commentService.createComment(newData)
+    window.location.reload()
   }
 
   if(!post) {
@@ -116,7 +116,7 @@ export const PostPage = () => {
 
         <ul className="space-y-4">
           {post.comments && post.comments.map(coment => (
-            <li className="flex items-center space-x-2 shadow">
+            <li className="flex items-center space-x-2 shadow" key={coment.post_id}>
               <img src="https://i.pinimg.com/564x/41/76/b9/4176b9b864c1947320764e82477c168f.jpg" alt="" className="w-10 h-10 rounded-full"/>
               <section>
                 <span className="text-lg font-semibold text-slate-700">{coment.name}</span>
@@ -129,7 +129,7 @@ export const PostPage = () => {
         <form className="flex flex-col space-y-2" onSubmit={handleCreateComment}>
           <input type="text" onChange={(e)=>setUser(e.target.value)} placeholder='Seu nome' className='px-5 py-2 border-solid border-2 border-slate-100 rounded-md utline outline-offset-2 outline-2 outline-slate-100'/>
           <textarea placeholder='Seu comentario' onChange={(e)=>setCommentValue(e.target.value)} className='px-5 py-2 border-solid border-2 border-slate-100 rounded-md utline outline-offset-2 outline-2 outline-slate-100'/>
-          <button className='px-5 py-2 bg-slate-400 rounded-md font-semibold shadow-md hover:bg-slate-500 active:bg-slate-400 active:ring focus:ring-slate-500'>Ver mais</button>
+          <button className='px-5 py-2 bg-slate-400 rounded-md font-semibold shadow-md hover:bg-slate-500 active:bg-slate-400 active:ring focus:ring-slate-500'>Publicar</button>
         </form>
 
 
