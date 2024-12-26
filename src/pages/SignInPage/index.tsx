abaa287e4a7cb4b108aab1dee6f2d3ffcfa1abfc
Faguim02 <fagnermuniz12@gmail.com>
2024-12-26 20:30:53 -0300

    style: colocando border radios no container

--- src/pages/SignInPage/index.tsx
+++ src/pages/SignInPage/index.tsx
@@ -55,16 +55,16 @@ export default function SignInPage() {
   return (
     <div className='flex flex-row justify-center items-center h-screen' style={{backgroundColor: '#ECEBE4'}}>
       
-      <img src={ilustration} alt="Preview" className='h-3/4'/>
+      <img src={ilustration} alt="Preview" className='h-3/4 rounded-l-md'/>
 
-      <form className="flex flex-col w-1/3 bg-white px-10 h-3/4 gap-4 justify-center" onSubmit={handleSubmit}>
+      <form className="flex flex-col w-1/3 bg-white px-10 h-3/4 gap-4 justify-center rounded-r-md" onSubmit={handleSubmit}>
         <h1 className='text-xl text-center font-bold'>Acessar conta</h1>
         <input type="email" placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} value={email} className='px-5 py-2 border-solid border-2 border-slate-100 rounded-md utline outline-offset-2 outline-2 outline-slate-100' required/>
         <input type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} value={password} className='px-5 py-2 border-solid border-2 border-slate-100 rounded-md utline outline-offset-2 outline-2 outline-slate-100' required/>
         <button type="submit" className='px-5 py-2 rounded-md font-semibold shadow-md hover:bg-slate-500 active:bg-slate-400 active:ring focus:ring-slate-500' style={{backgroundColor: '#7880B5'}}>
           {loading ? <Spinner size='sm' /> : 'Entrar'}
         </button>
-        <a href="" className='font-bold text-slate-800 text-center text-sm' >não tem uma conta ainda? click aqui</a>
+        <a href="/signUp" className='font-bold text-slate-800 text-center text-sm' >não tem uma conta ainda? click aqui</a>
       </form>
       
     </div>
