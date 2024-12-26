28d86a20093eede951d1a3eeca716bd63f64ccad
Faguim02 <fagnermuniz12@gmail.com>
2024-12-26 20:28:01 -0300

    ✨ feat: criando class CommentService que lida com os comentarios e o metodo 'createComment' que cria um comentario

--- src/services/api/commentService.ts
+++ src/services/api/commentService.ts
@@ -1,13 +1,41 @@
 import { commentDto } from "../../dtos/ComentDto";
+import { ErrorMessageDto } from "../../dtos/ErrorMessageDto";
 import Api from "./Api";
+import Cookies from "cookies-ts";
 
 export default class CommentService {
 
     private user = new Api().user;
 
-    async createComment(data: commentDto): Promise<commentDto> {
-        const dataRes = await this.user.post(`comment`, data);
+    async createComment(data: commentDto): Promise<boolean | ErrorMessageDto> {
 
-        return dataRes.data;
+        try {
+            let token = new Cookies().get("token");
+
+            const dataRes = await this.user.post(`comment`, data, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+
+            return true;
+            
+        } catch (error: any) {
+
+            if(error.response.status == 500) {
+                return {
+                    title: "Internal Server Error",
+                    message: "parece que sua internet não está tão boa",
+                    status: 500
+                } as ErrorMessageDto
+            }
+
+            return {
+                title: error.response.data.title,
+                message: error.response.data.message,
+                status: error.response.status
+            } as ErrorMessageDto
+        }
+
     }
 }
