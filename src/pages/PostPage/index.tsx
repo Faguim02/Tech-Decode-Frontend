da018ef47310cc3446f43096f2bf45f1164e045a
Faguim02 <fagnermuniz12@gmail.com>
2024-12-26 20:40:09 -0300

    refactor: adaptando a alteração da propriedade 'bannerUrl'

--- src/pages/PostPage/index.tsx
+++ src/pages/PostPage/index.tsx
@@ -10,7 +10,6 @@ export const PostPage = () => {
 
   const [post, setPost] = useState<postDto | null>(null)
   const [commentValue, setCommentValue] = useState<string>()
-  const [user, setUser] = useState<string>()
 
   const { id } = useParams() as {id: string}
 
@@ -27,9 +26,7 @@ export const PostPage = () => {
 
     const newData = {
       comment: commentValue,
-      name: user,
-      date: '0',
-      post_id: id
+      post: {id: id}
     } as commentDto
 
     const commentService = new CommentService()
@@ -116,10 +113,13 @@ export const PostPage = () => {
 
         <ul className="space-y-4">
           {post.comments && post.comments.map(coment => (
-            <li className="flex items-center space-x-2 shadow" key={coment.post_id}>
+            <li className="flex items-center space-x-2 shadow" key={coment.id}>
               <img src="https://i.pinimg.com/564x/41/76/b9/4176b9b864c1947320764e82477c168f.jpg" alt="" className="w-10 h-10 rounded-full"/>
               <section>
-                <span className="text-lg font-semibold text-slate-700">{coment.name}</span>
+                <div className="flex items-center space-x-2">
+                  <span className="text-lg font-semibold text-slate-700">{coment.username}</span>
+                  <span className="text-sm text-slate-600">{coment.date_at}</span>
+                </div>
                 <p className="text-slate-600">{coment.comment}</p>
               </section>
             </li>
@@ -127,7 +127,6 @@ export const PostPage = () => {
         </ul>
 
         <form className="flex flex-col space-y-2" onSubmit={handleCreateComment}>
-          <input type="text" onChange={(e)=>setUser(e.target.value)} placeholder='Seu nome' className='px-5 py-2 border-solid border-2 border-slate-100 rounded-md utline outline-offset-2 outline-2 outline-slate-100'/>
           <textarea placeholder='Seu comentario' onChange={(e)=>setCommentValue(e.target.value)} className='px-5 py-2 border-solid border-2 border-slate-100 rounded-md utline outline-offset-2 outline-2 outline-slate-100'/>
           <button className='px-5 py-2 bg-slate-400 rounded-md font-semibold shadow-md hover:bg-slate-500 active:bg-slate-400 active:ring focus:ring-slate-500'>Publicar</button>
         </form>
