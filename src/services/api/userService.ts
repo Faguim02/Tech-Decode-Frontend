24c3856f88543554441889a66a55f358b85b97a6
Faguim02 <fagnermuniz12@gmail.com>
2024-12-28 16:21:30 -0300

    ✨ feat: nova função de criação de usuario admin

--- src/services/api/userService.ts
+++ src/services/api/userService.ts
@@ -4,12 +4,13 @@ import Api from "./Api";
 import Cookies from 'cookies-ts'
 
 export class UserService {
-    private admin = new Api().user;
+    private admin = new Api().admin;
+    private user = new Api().user;
 
     public async signIn(userDto: userDto): Promise<boolean | ErrorMessageDto> {
         
         try {
-            let token = await this.admin.post('auth/signIn', userDto);
+            let token = await this.user.post('auth/signIn', userDto);
 
             console.log(token.data)  
 
@@ -32,8 +33,7 @@ export class UserService {
 
     public async signUp(userDto: userDto): Promise<boolean | ErrorMessageDto> {
         try {
-            userDto.role = 'COMMON';
-            await this.admin.post('auth/signUp', userDto);
+            await this.user.post('auth/signUp', userDto);
 
             return true;
         } catch (error: any) {
@@ -43,6 +43,37 @@ export class UserService {
                     title: "Internal Server Error",
                     message: "parece que sua internet não está tão boa",
                     status: 500
+                } as ErrorMessageDto
+            }
+
+            return {
+                title: error.response.data.title,
+                message: error.response.data.message,
+                status: error.response.status
+            } as ErrorMessageDto
+        }
+    }
+
+    public async signUpAdmin(userDto: userDto): Promise<boolean | ErrorMessageDto> {
+        try {
+            await this.admin.post('auth/signUp/admin', userDto);
+
+            return true;
+        } catch (error: any) {
+
+            if(error.response.status == 500) {
+                return {
+                    title: "Internal Server Error",
+                    message: "parece que sua internet não está tão boa",
+                    status: 500
+                } as ErrorMessageDto
+            }
+
+            if(error.response.status == 403) {
+                return {
+                    title: "Não autorizado",
+                    message: "Você não possui acesso para criar contas de administrador",
+                    status: 403
                 } as ErrorMessageDto
             }
 
