2fb64ff677f406d0fc9fc48a0460fc19666266a7
Faguim02 <fagnermuniz12@gmail.com>
2024-12-29 19:47:59 -0300

    ✨ feat: adicionando função para criar nova categoria

--- src/services/api/categoryService.ts
+++ src/services/api/categoryService.ts
@@ -1,4 +1,5 @@
 import { categoryDto } from "../../dtos/CategoryDto"
+import { ErrorMessageDto } from "../../dtos/ErrorMessageDto";
 import Api from "./Api"
 
 export class CategoryService {
@@ -11,6 +12,31 @@ export class CategoryService {
     } catch (error: any) {
 
         return null;
+        
+    }
+  }
+
+  async createCategory(data: categoryDto): Promise<boolean | ErrorMessageDto> {
+    try {
+        
+        const response = await new Api().admin.post('category', data)
+        return true;
+
+    } catch (error: any) {
+
+        if(error.response.status == 500) {
+            return {
+                title: "Internal Server Error",
+                message: "parece que sua internet não está tão boa",
+                status: 500
+            } as ErrorMessageDto
+        }
+
+        return {
+            title: error.response.data.title,
+            message: error.response.data.message,
+            status: error.response.status
+        } as ErrorMessageDto
         
     }
   }
