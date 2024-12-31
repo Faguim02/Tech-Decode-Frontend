0f1a6987ae1c33a989a99a683159a57b254f2416
Faguim02 <fagnermuniz12@gmail.com>
2024-12-30 22:36:31 -0300

    ♻️ refactor: mudando forma que é tratado os erros

--- src/services/api/categoryService.ts
+++ src/services/api/categoryService.ts
@@ -16,11 +16,15 @@ export class CategoryService {
     }
   }
 
-  async createCategory(data: categoryDto): Promise<boolean | ErrorMessageDto> {
+  async createCategory(data: categoryDto): Promise<ErrorMessageDto> {
     try {
         
-        await new Api().admin.post('category', data)
-        return true;
+        await new Api().admin.post('/category', data)
+        return {
+            status: 201,
+            title: "Categoria criada com sucesso",
+            message: "A categoria foi criada com sucesso"
+        } as ErrorMessageDto;
 
     } catch (error: any) {
 
@@ -32,6 +36,14 @@ export class CategoryService {
             } as ErrorMessageDto
         }
 
+        if(error.response.status == 403) {
+            return {
+                title: "forbidden",
+                message: "você não tem permissão para fazer isso",
+                status: 403
+            } as ErrorMessageDto
+        }
+
         return {
             title: error.response.data.title,
             message: error.response.data.message,
@@ -40,4 +52,40 @@ export class CategoryService {
         
     }
   }
+
+async deleteCategory(id: string): Promise<ErrorMessageDto> {
+    try {        
+        await new Api().admin.delete(`/category/${id}`)
+        return {
+            status: 200,
+            title: "Categoria deletada com sucesso",
+            message: "A categoria foi deletada com sucesso"
+        } as ErrorMessageDto;
+
+    } catch (error: any) {
+
+        if(error.response.status == 500) {
+            return {
+                title: "Internal Server Error",
+                message: "parece que sua internet não está tão boa",
+                status: 500
+            } as ErrorMessageDto
+        }
+
+        if(error.response.status == 403) {
+            return {
+                title: "forbidden",
+                message: "você não tem permissão para fazer isso",
+                status: 403
+            } as ErrorMessageDto
+        }
+
+        return {
+            title: error.response.data.title,
+            message: error.response.data.message,
+            status: error.response.status
+        } as ErrorMessageDto
+        
+    }
+}
 }
