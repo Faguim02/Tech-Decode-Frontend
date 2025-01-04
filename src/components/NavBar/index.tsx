0863613159b584def8a41f3b570e83576210f5b2
Faguim02 <fagnermuniz12@gmail.com>
2025-01-03 22:58:36 -0300

    🖌️ feat: deixando telas responsivas

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,8 +1,10 @@
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import logo from '../../assets/logo.svg'
 import { FaRegUser, FaSignOutAlt } from "react-icons/fa";
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Cookies from 'cookies-ts'
+import { Drawer, DrawerBody, DrawerContent, DrawerFooter, DrawerOverlay, useDisclosure } from '@chakra-ui/react';
+import { MdOutlineMenu } from 'react-icons/md';
 
 export const NavBarComponent = () => {
 
@@ -10,6 +12,9 @@ export const NavBarComponent = () => {
     const [isAuth, setIsAuth] = useState<boolean>(false)
 
     const navigate = useNavigate()
+
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const btnRef = React.useRef()
 
     useEffect(()=>{
         let token = new Cookies().get('token')
@@ -33,21 +38,7 @@ export const NavBarComponent = () => {
             <img src={logo} alt="logo" />
         </figure>
 
-        <nav className='px-5 py-2 shadow-md rounded-md'>
-            <ul className='flex gap-8'>
-                <li>
-                    <Link to={"/"} className='text-slate-600 font-semibold focus:text-blue-500'> <span>•</span> Inicio</Link>
-                </li>
-                <li>
-                    <Link to={"/contact"} className='text-slate-600 font-semibold focus:text-blue-500'> <span>•</span> Contato</Link>
-                </li>
-                <li>
-                    <Link to={"/about"} className='text-slate-600 font-semibold focus:text-blue-500'> <span>•</span> Sobre</Link>
-                </li>
-            </ul>
-        </nav>
-
-        <div className='flex gap-4'>
+        <div className='gap-4 hidden md:flex'>
             <form onSubmit={handleSearchPost}>
                 <input type="text" onChange={(e)=>setSearch(e.target.value)} placeholder='Pesquisar' className='px-5 py-2 border-solid border-2 border-slate-100 rounded-md utline outline-offset-2 outline-2 outline-slate-100'/>
             </form>
@@ -66,6 +57,41 @@ export const NavBarComponent = () => {
             )}
             
         </div>
+        <button ref={btnRef.current} onClick={onOpen} className='md:hidden'>
+            <MdOutlineMenu size={30}/>
+        </button>
+        <Drawer
+            isOpen={isOpen}
+            placement='right'
+            onClose={onClose}
+            finalFocusRef={btnRef.current}
+        >
+            <DrawerOverlay />
+            <DrawerContent>
+            
+
+            <DrawerBody>
+                <form onSubmit={handleSearchPost}>
+                    <input type="text" onChange={(e)=>setSearch(e.target.value)} placeholder='Pesquisar' className='px-5 py-2 border-solid border-2 border-slate-100 rounded-md utline outline-offset-2 outline-2 outline-slate-100'/>
+                </form>
+            </DrawerBody>
+
+            <DrawerFooter>
+            {isAuth ? (
+                <button onClick={()=>{
+                    new Cookies().remove("token")
+                    setIsAuth(false)
+                }} className='px-5 py-3 bg-slate-400 rounded-md font-semibold shadow-md hover:bg-slate-500 active:bg-slate-400 active:ring focus:ring-slate-500 w-full flex items-center justify-center gap-2'>
+                    <FaSignOutAlt /> Sair
+                    </button>
+                ): (
+                    <button onClick={()=>navigate('signIn')} className='px-5 py-3 bg-slate-400 rounded-md font-semibold shadow-md hover:bg-slate-500 active:bg-slate-400 active:ring focus:ring-slate-500 w-full flex items-center justify-center gap-2'>
+                        <FaRegUser/> Entrar
+                    </button>
+                )}
+            </DrawerFooter>
+            </DrawerContent>
+        </Drawer>
     </nav>
   )
 }
