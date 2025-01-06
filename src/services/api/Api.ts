217e7f1eca792b31d02b7f2115810323457f00b5
Faguim02 <fagnermuniz12@gmail.com>
2025-01-06 11:59:00 -0300

    ♻️  refactor: alterando a base url

--- src/services/api/Api.ts
+++ src/services/api/Api.ts
@@ -5,11 +5,11 @@ export default class Api {
     private adminCookie = new Cookies().get('token')
 
     public user = axios.create({
-        baseURL: 'http://ec2-54-209-212-173.compute-1.amazonaws.com/',
+        baseURL: '/api',
     })
 
     public admin = axios.create({
-        baseURL: 'http://ec2-54-209-212-173.compute-1.amazonaws.com/',
+        baseURL: '/api',
         headers: {
             Authorization: `Bearer ${this.adminCookie}`
         }
