71fe0c383860429243454f895cdb05a159165c08
Faguim02 <fagnermuniz12@gmail.com>
2025-01-24 18:05:19 -0300

    ✨feat: adicionando valores aos admin e users

--- src/pages/DashBoardPage/index.tsx
+++ src/pages/DashBoardPage/index.tsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react";
 import LeftNavigation from "../../components/LeftNavigation";
+import { findAllUsersDto } from "../../dtos/FindUsersDto";
+import { UserService } from "../../services/api/userService";
 
 export default function DashBoardPage() {
+
+  const [users, setUsers] = useState<findAllUsersDto>();
+  const [admins, setAdmins] = useState<findAllUsersDto>();
+
+  useEffect(() => {
+    (async () => {
+      const users = await new UserService().findAllUsers();
+      const admins = await new UserService().findAllAdmin();
+
+      if(users as findAllUsersDto) {
+        setUsers(users as findAllUsersDto);
+        setAdmins(admins as findAllUsersDto);
+      }
+
+    })()
+  }, []);
+
   return (
     <div style={{backgroundColor: "#ECEBE4"}} className="h-screen flex p-8">
       <LeftNavigation/>
@@ -21,7 +41,11 @@ export default function DashBoardPage() {
             </div>
             <div className="bg-white p-4 rounded-md shadow-md flex flex-col items-center gap-2">
                 <h3 className="text-lg">Usuarios</h3>
-                <span className="font-bold">5</span>
+                <span className="font-bold">{users?.size}</span>
+            </div>
+            <div className="bg-white p-4 rounded-md shadow-md flex flex-col items-center gap-2">
+                <h3 className="text-lg">Usuarios</h3>
+                <span className="font-bold">{admins?.size}</span>
             </div>
             <div className="bg-white p-4 rounded-md shadow-md flex flex-col items-center gap-2">
                 <h3 className="text-lg">Comentarios</h3>
